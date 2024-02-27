b3e8b83932add8d0698cf67a295f3daf6b1d4d9a
Jani Kavander <jkavander@copyright.com>
2024-02-27 11:01:10 +0200

    duplicate code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-      System.out.println( "Hello World!" );
-    System.out.println( "This is a new line!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "This is a new line!" );
+        printDuplicateMessage();
+    }
+    
+    // Intentionally duplicating code to trigger SonarQube rule
+    public static void printDuplicateMessage() {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is a new line!" );
     }
 }
