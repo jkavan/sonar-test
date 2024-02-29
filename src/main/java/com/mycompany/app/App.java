0d45df446313fd670f3e812bbb6bc472de3e7b6e
Jani Kavander <jkavander@copyright.com>
2024-02-29 08:22:20 +0200

    abc

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,12 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.err.println("Hello");
     }
-        
-    private static void unusedMethod() {
-        System.out.println("This method is never used.");
-    }
-
-    public String exposedField;
-
 }
