eff6b6411255ee6eb2398f35162b2cf811c01cb8
Jani Kavander <jkavander@copyright.com>
2024-02-29 08:13:08 +0200

    remove lines

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,33 +10,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        System.out.println( "This is a new line!" );
-        login();
         System.err.println("Hello");
     }
     
-    // Simulating hard-coded credentials to trigger SonarQube rule
-    public static void login() {
-        String username = "admin";
-        String password = "password123"; // Hard-coded password
-        System.out.println("Hello " + username + ". Password: " + password);
-    }
-
     private static void unusedMethod() {
         System.out.println("This method is never used.");
     }
